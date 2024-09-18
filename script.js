f301417c6e4e63d0e96631412f4d6294a0184203
Ahmed Hafez <ahmedhafezx7@outlook.com>
2024-09-18 15:09:58 +0300

    Create function to generate messages

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-    console.log("Hello  World!")
+const msg = ["You only live once!", "Believe in yourself", "Work hard play hard", "If it was easy then it is not worth it", "Rise above all"];
+
+const generateMessage = Arr => {
+    let message = Arr[Math.floor(Math.random() * Arr.length - 1)];
+    console.log(message, Arr.indexOf(message));
+}
+
+generateMessage(msg);
