4913bbf2347435588bf3d5e430a1454504fbbd68
Ahmed Hafez <ahmedhafezx7@outlook.com>
2024-09-18 15:10:24 +0300

    Fix bug of Arrr element

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const msg = ["You only live once!", "Believe in yourself", "Work hard play hard", "If it was easy then it is not worth it", "Rise above all"];
 
 const generateMessage = Arr => {
-    let message = Arr[Math.floor(Math.random() * Arr.length - 1)];
+    let message = Arr[Math.floor(Math.random() * Arr.length)];
     console.log(message, Arr.indexOf(message));
 }
 
